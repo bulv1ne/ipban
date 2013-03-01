3ea256125764fbb6d0567c1de672426373496e1c
Niels Lemmens <niels.lemmens@dinwebbstrateg.se>
2013-03-01 08:46:24 +0100

    Update wrapper.py
    Add comments
    Replace render_to_response with HttpResponse

--- wrapper.py
+++ wrapper.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render_to_response
+from django.http import HttpResponse
 from models import *
 import datetime
 
@@ -6,25 +6,26 @@ def ipban(f):
 	def wrap(request, *args, **kwargs):
 		ip, created = IP.objects.get_or_create(ip=request.META['REMOTE_ADDR'])
 		if created:
+            # 10 Seconds span
 			ip.time = datetime.datetime.now() + datetime.timedelta(0, 10, 0)
 		ip.requests += 1
 		ip.save()
 		if ip.banned_for_life:
-			return render_to_response('ipban.html', {
-				'ip': ip,
-			})
+            return HttpResponse('You have been blocked for life!')
+        # Maximum 10 requests allowed
 		max_requests = 10
 		if ip.requests >= max_requests:
 			if datetime.datetime.now() < ip.time:
-				if ip.requests == max_requests:
-					ip.ban_counter += 1
-					ip.time = datetime.datetime.now() + datetime.timedelta(0, 10, 0)
-					ip.save()
-				return render_to_response('ipban.html', {
-					'ip': ip,
-				})
+                # Too many requests in soo little time
+                ip.ban_counter += 1
+                # 10 Seconds span
+                ip.time = datetime.datetime.now() + datetime.timedelta(0, 10, 0)
+                ip.save()
+                return HttpResponse('You have been blocked for 10 seconds!')
 			else:
+                # Reset the ip
 				ip.requests = 0
+                # 10 Seconds span
 				ip.time = datetime.datetime.now() + datetime.timedelta(0, 10, 0)
 				ip.save()
 		return f(request, *args, **kwargs)
