8172fc3fc1073e98d8b922ce1e6abeb141ccc27d
Bulv1ne <draso.odin@gmail.com>
2013-04-05 09:29:08 +0200

    Change tabs by spaces

--- models.py
+++ models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 class IP(models.Model):
-	ip = models.CharField(max_length=127)
-	time = models.DateTimeField(null=True, default=None)
-	requests = models.IntegerField(default=0)
-	ban_counter = models.IntegerField(default=0)
-	banned_for_life = models.BooleanField(default=False)
+    ip = models.CharField(max_length=127)
+    time = models.DateTimeField(null=True, default=None)
+    requests = models.IntegerField(default=0)
+    ban_counter = models.IntegerField(default=0)
+    banned_for_life = models.BooleanField(default=False)
 
-	def __unicode__(self):
-		if self.banned_for_life:
-			return self.ip + ' (Permanent ban)'
-		return self.ip + ' Bans: ' + str(self.ban_counter)
+    def __unicode__(self):
+        if self.banned_for_life:
+            return self.ip + ' (Permanent ban)'
+        return self.ip + ' Bans: ' + str(self.ban_counter)
--- wrapper.py
+++ wrapper.py
@@ -3,32 +3,32 @@ from models import *
 import datetime
 
 def ipban(f):
-	def wrap(request, *args, **kwargs):
-		ip, created = IP.objects.get_or_create(ip=request.META['REMOTE_ADDR'])
-		if created:
+    def wrap(request, *args, **kwargs):
+        ip, created = IP.objects.get_or_create(ip=request.META['REMOTE_ADDR'])
+        if created:
             # 10 Seconds span
-			ip.time = datetime.datetime.now() + datetime.timedelta(0, 10, 0)
-		ip.requests += 1
-		ip.save()
-		if ip.banned_for_life:
+            ip.time = datetime.datetime.now() + datetime.timedelta(0, 10, 0)
+        ip.requests += 1
+        ip.save()
+        if ip.banned_for_life:
             return HttpResponse('You have been blocked for life!')
         # Maximum 10 requests allowed
-		max_requests = 10
-		if ip.requests >= max_requests:
-			if datetime.datetime.now() < ip.time:
+        max_requests = 10
+        if ip.requests >= max_requests:
+            if datetime.datetime.now() < ip.time:
                 # Too many requests in soo little time
                 ip.ban_counter += 1
                 # 10 Seconds span
                 ip.time = datetime.datetime.now() + datetime.timedelta(0, 10, 0)
                 ip.save()
                 return HttpResponse('You have been blocked for 10 seconds!')
-			else:
+            else:
                 # Reset the ip
-				ip.requests = 0
+                ip.requests = 0
                 # 10 Seconds span
-				ip.time = datetime.datetime.now() + datetime.timedelta(0, 10, 0)
-				ip.save()
-		return f(request, *args, **kwargs)
-	wrap.__doc__=f.__doc__
-	wrap.__name__=f.__name__
-	return wrap
+                ip.time = datetime.datetime.now() + datetime.timedelta(0, 10, 0)
+                ip.save()
+        return f(request, *args, **kwargs)
+    wrap.__doc__=f.__doc__
+    wrap.__name__=f.__name__
+    return wrap
